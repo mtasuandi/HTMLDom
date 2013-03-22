3e5f349207ea8bf04fa1a0bb08c36e292c5281dc
M Teguh A Suandi <teguh.andro@gmail.com>
2013-03-23 02:01:05 +0700

    Update dom.php
    returned to array

--- dom.php
+++ dom.php
@@ -6,17 +6,25 @@ $content = <<<HTML
 <input type='text' id='email' name='email' />
 <input type='hidden' id='hidden1' name='hidden1' value='hiddenvalue1'/>
 <input type='hidden' id='hidden2' name='hidden2' value='hiddenvalue2'/>
+<input type='hidden' id='hidden3' name='hidden3' value='hiddenvalue3'/>
 </form>
 HTML;
 
 $document   = new DOMDocument();
 $document->loadHTML($content);
-$input 		= $document->getElementsByTagName('input')->item(0);
-$form		= $document->getElementsByTagName('form')->item(0);
-echo $form->attributes->getNamedItem("action")->value;
-echo '<br/>';
-echo $form->attributes->getNamedItem("method")->value;
-echo '<br/>';
-echo $input->attributes->getNamedItem("name")->value;
-echo $document->getElementsByTagName('input')->length;
+
+for($i=0; $i<$document->getElementsByTagName('input')->length; $i++)
+{
+	# Input
+	$input 			= $document->getElementsByTagName('input')->item($i);
+	$name[]['input']= $input->attributes->getNamedItem('name')->value;
+}
+
+# Form Attr
+$form				= $document->getElementsByTagName('form')->item(0);
+$action['action']	= $form->attributes->getNamedItem('action')->value;
+$method['method']	= $form->attributes->getNamedItem('method')->value;
+
+$arr 	= array_merge($name, $action, $method);
+var_dump($arr);
 ?>
